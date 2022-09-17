93ad9cff6269648fba265a959651c55e38702a79
Дмитрий Старостенок <49001181+Hlebnica@users.noreply.github.com>
2022-09-17 12:51:35 +0500

    Новый способ создания таблицы поставок, который делается по заданию

---
+++ CreateTableSupplyVersion2.sql
@@ -0,0 +1,10 @@
+CREATE TABLE supplies(
+	id_provider INT NOT NULL,
+	id_detail INT NOT NULL,
+	id_project INT NOT NULL,
+	supplies_quantity INT NOT NULL CHECK(supplies_quantity>0),
+	PRIMARY KEY(id_provider, id_detail, id_project),
+	FOREIGN KEY (id_provider) REFERENCES providers(id_provider) ON DELETE CASCADE,
+	FOREIGN KEY (id_detail) REFERENCES details(id_detail) ON DELETE CASCADE,
+	FOREIGN KEY (id_project) REFERENCES projects(id_project) ON DELETE CASCADE
+)
