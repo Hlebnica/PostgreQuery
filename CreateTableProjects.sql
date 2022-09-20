80a34520f202c6bb2ecbb8eef5c80ec025338316
Дмитрий Старостенок <49001181+Hlebnica@users.noreply.github.com>
2022-09-20 12:47:12 +0500

    AddCascadeUpdate

--- CreateTableProjects.sql
+++ CreateTableProjects.sql
@@ -4,5 +4,7 @@ CREATE TABLE projects(
 	id_city INT NOT NULL,
 	address VARCHAR(120),
 	budget INT NOT NULL CHECK(budget>0),
-	FOREIGN KEY (id_city)  REFERENCES cities(id_city) ON DELETE CASCADE
+	FOREIGN KEY (id_city)  REFERENCES cities(id_city) 
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
 )
--- CreateTableProvider.sql
+++ CreateTableProvider.sql
@@ -4,7 +4,9 @@ CREATE TABLE providers(
 	id_city INT NOT NULL,
 	provider_address VARCHAR(120) DEFAULT 'Неизвестен' NOT NULL,
 	provider_rating INT NOT NULL CHECK (provider_rating > 0 AND provider_rating <= 10),
-	FOREIGN KEY (id_city)  REFERENCES cities(id_city) ON DELETE CASCADE,
+	FOREIGN KEY (id_city)  REFERENCES cities(id_city) 
+	ON DELETE CASCADE
+	ON UPDATE CASCADE,
 	CONSTRAINT Same_provider_name_in_city
 	UNIQUE (provider_name, id_city)
 )
