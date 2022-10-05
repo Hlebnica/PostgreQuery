980d007f082f448e40dc198834b8e28c1af3a9f4
Дмитрий Старостенок <49001181+Hlebnica@users.noreply.github.com>
2022-10-05 11:38:58 +0500

    ОтчетEnd-6

---
+++ Task6/Economist/1/ProviderListAll_1.sql
@@ -0,0 +1,2 @@
+SELECT id_provider, provider_name, id_city, provider_address, provider_rating
+FROM providers;
---
+++ Task6/Economist/2/DetailsListAll_2.sql
@@ -0,0 +1,2 @@
+SELECT id_detail, detail_name, detail_price, detail_color, detail_weight
+FROM details;
