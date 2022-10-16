76d76909f2aa26f320eca0d8b5f04860dad7cd75
Дмитрий Старостенок <49001181+Hlebnica@users.noreply.github.com>
2022-10-16 14:11:46 +0500

    Report_Task6__Fix

--- Task6/Director/2/DetailsListAll_2.sql
+++ Task6/Director/2/DetailsListAll_2.sql
@@ -6,5 +6,4 @@ SELECT
 	SUM(supplies.supplies_quantity * details.detail_price) AS "Общая сумма поставок"
 FROM details
 	INNER JOIN supplies ON supplies.id_detail = details.id_detail
-	INNER JOIN details ON details.id_detail = supplies.id_detail
 GROUP BY detail_name, detail_price, detail_color, detail_weight
--- Task6/Director/3/ProjectListMAXbudget_3.sql
+++ Task6/Director/3/ProjectListMAXbudget_3.sql
@@ -3,7 +3,7 @@ SELECT
 	budget AS "Бюджет",
 	SUM(supplies.supplies_quantity * details.detail_price) AS "Общая сумма поставок"
 FROM projects
-	INNER JOIN supplies ON supplies.id_project = projects.
+	INNER JOIN supplies ON supplies.id_project = projects.id_project
 	INNER JOIN details ON details.id_detail = supplies.id_detail
 GROUP BY project_name, budget
 HAVING (projects.budget = (SELECT MAX(projects.budget) FROM projects))
