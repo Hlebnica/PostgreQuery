553b4af8d05d2814b5824ce40b01055543ddf1f0
Дмитрий Старостенок <apple260500@gmail.com>
2022-11-29 16:21:21 +0500

    CreateFix

--- CREATE/CreateTableSupply.sql
+++ CREATE/CreateTableSupply.sql
@@ -1,10 +1,16 @@
 CREATE TABLE supplies(
-	id_supply INT PRIMARY KEY NOT NULL,
-	id_provider INT NOT NULL,
-	id_detail INT NOT NULL,
-	id_project INT NOT NULL,
+	id_provider SERIAL,
+	id_detail SERIAL,
+	id_project SERIAL,
 	supplies_quantity INT NOT NULL CHECK(supplies_quantity>0),
-	FOREIGN KEY (id_provider) REFERENCES providers(id_provider) ON DELETE CASCADE,
-	FOREIGN KEY (id_detail) REFERENCES details(id_detail) ON DELETE CASCADE,
-	FOREIGN KEY (id_project) REFERENCES projects(id_project) ON DELETE CASCADE
+	PRIMARY KEY(id_provider, id_detail, id_project),
+	FOREIGN KEY (id_provider) REFERENCES providers(id_provider) 
+	ON DELETE CASCADE
+	ON UPDATE CASCADE,
+	FOREIGN KEY (id_detail) REFERENCES details(id_detail) 
+	ON DELETE CASCADE
+	ON UPDATE CASCADE,
+	FOREIGN KEY (id_project) REFERENCES projects(id_project) 
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
 )
